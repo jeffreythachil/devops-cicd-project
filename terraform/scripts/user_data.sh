c18c96782d70214ab961455aab2cde5c7044356e
Jeffrey Thomas <jeffrey.thachil@gmail.com>
2026-08-12 10:15:13 +0000

    latest

--- terraform/scripts/user_data.sh
+++ terraform/scripts/user_data.sh
@@ -1,9 +1,65 @@
 #!/bin/bash
 
+set -e
+
+##############################################
+# Update Packages
+##############################################
+
 apt update -y
+
+##############################################
+# Install Docker
+##############################################
+
 apt install -y docker.io
 
 systemctl enable docker
 systemctl start docker
 
 usermod -aG docker ubuntu
+
+##############################################
+# Install AWS CLI
+##############################################
+
+apt install -y unzip curl
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+unzip awscliv2.zip
+
+./aws/install
+
+##############################################
+# Login to Amazon ECR
+##############################################
+
+REGION="ap-south-1"
+
+ACCOUNT_ID="163265929593"
+
+REPOSITORY="devops-cicd-project"
+
+aws ecr get-login-password --region $REGION | \
+docker login \
+--username AWS \
+--password-stdin \
+${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com
+
+##############################################
+# Pull Latest Image
+##############################################
+
+docker pull \
+${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${REPOSITORY}:latest
+
+##############################################
+# Run Container
+##############################################
+
+docker run -d \
+--name flask-app \
+--restart unless-stopped \
+-p 80:5000 \
+${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${REPOSITORY}:latest

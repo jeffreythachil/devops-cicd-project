34efbea88fbe0bd1ec070d6599d7a3420ca707ce
Jeffrey Thomas <jeffrey.thachil@gmail.com>
2026-08-13 10:29:41 +0000

    Add application health endpoint

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "Hello DevOps!"
+    return "Hello from DevOps CI/CD Project!"
+
+
+@app.route("/health")
+def health():
+    return {"status": "healthy"}, 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

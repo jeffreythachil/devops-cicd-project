9742b56d805d92ea8471407b303d4c7f178aaa65
Jeffrey Thomas <jeffrey.thachil@gmail.com>
2026-08-13 10:34:38 +0000

    Fix home endpoint unit test

--- test_app.py
+++ test_app.py
@@ -1,9 +1,19 @@
 from app import app
 
+
 def test_home():
     client = app.test_client()
 
     response = client.get("/")
 
     assert response.status_code == 200
-    assert response.data == b"Hello DevOps!"
+    assert response.data == b"Hello from DevOps CI/CD Project!"
+
+
+def test_health():
+    client = app.test_client()
+
+    response = client.get("/health")
+
+    assert response.status_code == 200
+    assert response.json["status"] == "healthy"
